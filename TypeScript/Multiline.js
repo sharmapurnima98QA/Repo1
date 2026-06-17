aa79923422fd445e8f22077cfe33a1a319057540
Purnima <sharmapurnima98@gmail.com>
2026-06-17 21:21:46 +0530

    multiline message printing

---
+++ TypeScript/Multiline.js
@@ -0,0 +1,9 @@
+"use strict";
+let m1 = "hi! this is my First line of code";
+//this file contains use of multilines and comments;
+console.log(`${m1}
+    "using backticks or template literals i printed second line"
+    "printing multiple lines using template lterals"`);
+let Uname = "Bob the builder";
+// use of concatenation operator "+" and new line character "/n"
+console.log("\n other method \n new user added with name: " + Uname + "\nand motto : KAR KE DIKHAYENGE! HAN BHAI HAN!" + "with status = Active\nuse concatenation operator " + " and new line character");
---
+++ TypeScript/Multiline.ts
@@ -0,0 +1,11 @@
+let m1: string = "hi! this is my First line of code";
+
+//this file contains use of multilines and comments;
+console.log (`${m1}
+    "using backticks or template literals i printed second line"
+    "printing multiple lines using template lterals"`
+);
+let Uname : string = "Bob the builder";
+// use of concatenation operator "+" and new line character "/n"
+console.log ("\n other method \n new user added with name: " + Uname + "\nand motto : KAR KE DIKHAYENGE! HAN BHAI HAN!" + "with status = Active\nuse concatenation operator "+" and new line character");
+
