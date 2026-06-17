93d6905c24ce409625c2a596d9e495fff53676d0
Purnima <sharmapurnima98@gmail.com>
2026-06-17 21:34:01 +0530

    basic if program with 0 logic

---
+++ TypeScript/decisionMakingIFThen.js
@@ -0,0 +1,8 @@
+"use strict";
+let n1 = 2, n2 = 3, n3 = 4;
+if (n1 * n2 * 10 > 50)
+    console.log("above 50%");
+if (n3 * n2 * 10 > 100)
+    console.log("above 100%");
+if (n3 * n2 * 10 - 20 <= 100)
+    console.log("less than or equal to 100%");
---
+++ TypeScript/decisionMakingIFThen.ts
@@ -0,0 +1,7 @@
+let n1 : number=2, n2:number=3, n3: number =4;
+if (n1*n2*10 > 50)
+    console.log("above 50%");
+if (n3*n2*10 > 100)
+    console.log("above 100%");
+if (n3*n2*10 -20 <= 100)
+    console.log("less than or equal to 100%");
