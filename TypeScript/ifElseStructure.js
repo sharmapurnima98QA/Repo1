e99cc718b0e590280123cb781f31b07866e8ddaa
Purnima <sharmapurnima98@gmail.com>
2026-06-18 10:15:09 +0530

    ifElseRevised

---
+++ TypeScript/ifElseStructure.js
@@ -0,0 +1,11 @@
+"use strict";
+let n1 = "userName1", n2 = "name2", n3 = "name3";
+if (n1 == "uname") {
+    console.log(n1);
+}
+else if (n2 == "name2") {
+    console.log(n2);
+}
+else if (n3 == "name3") {
+    console.log(n3);
+}
