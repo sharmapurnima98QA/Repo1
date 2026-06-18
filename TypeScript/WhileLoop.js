db836257a409789f7c14a25060e11d6a855469f0
Purnima <sharmapurnima98@gmail.com>
2026-06-18 12:47:37 +0530

    while loop revised

---
+++ TypeScript/WhileLoop.js
@@ -0,0 +1,9 @@
+"use strict";
+let i = 1;
+while (i <= 5) {
+    console.log("printing value of i using while loop where i =", i);
+    i++;
+    if (i == 5) {
+        console.log("while loop executed successfully and ended here");
+    }
+}
---
+++ TypeScript/WhileLoop.ts
@@ -0,0 +1,10 @@
+let i:number=1;
+while(i<=5)
+{
+    console.log("printing value of i using while loop where i =",i);
+    i++;
+    if(i==5)
+    {
+        console.log("while loop executed successfully and ended here");
+    }
+}
